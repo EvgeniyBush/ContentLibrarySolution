57f856974f1b2b207f36dc895180555cc90f7b07
EvgeniyBush <8472235@stud.nau.edu.ua>
2025-04-19 23:56:03 +0300

    Add pattern UnitOfWork

---
+++ DAL/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿#pragma warning disable 
+
+using DAL2.Entities;
+
+namespace DAL2.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Content> Contents { get; }
+        IGenericRepository<Storage> Storages { get; }
+        IGenericRepository<ContentLocation> ContentLocations { get; }
+
+        Task<int> CompleteAsync();
+    }
+}
---
+++ DAL/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿#pragma warning disable 
+
+using DAL2.Entities;
+
+namespace DAL2.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public IGenericRepository<Content> Contents { get; }
+        public IGenericRepository<Storage> Storages { get; }
+        public IGenericRepository<ContentLocation> ContentLocations { get; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            Contents = new GenericRepository<Content>(_context);
+            Storages = new GenericRepository<Storage>(_context);
+            ContentLocations = new GenericRepository<ContentLocation>(_context);
+        }
+
+        public async Task<int> CompleteAsync() => await _context.SaveChangesAsync();
+
+        public void Dispose() => _context.Dispose();
+    }
+}
