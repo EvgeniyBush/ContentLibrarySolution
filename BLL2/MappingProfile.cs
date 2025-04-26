adbb51c4a25638588cba37e0c03a34613b149003
EvgeniyBush <8472235@stud.nau.edu.ua>
2025-04-26 22:25:51 +0300

    Mapping class

---
+++ BLL2/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿#pragma warning disable 
+
+using AutoMapper;
+using BLL2.DTO;
+using DAL2.Entities;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Book, BookDto>().ReverseMap();
+        CreateMap<Audio, AudioDto>().ReverseMap();
+        CreateMap<Video, VideoDto>().ReverseMap();
+        CreateMap<Document, DocumentDto>().ReverseMap();
+
+        CreateMap<Storage, StorageDto>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.LocationName, opt => opt.MapFrom(src => src.LocationName));
+    }
+}
